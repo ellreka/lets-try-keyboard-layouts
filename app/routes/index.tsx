e225170f1c43a5b706f55854886abeee2f343a11
ellreka <ellreka5364@gmail.com>
2022-12-24 04:01:45 +0900

    feat: init

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold font-mono text-[#333]">
+        Let's Try Keyboard Layouts !
+      </h1>
+    </div>
+  );
+}
