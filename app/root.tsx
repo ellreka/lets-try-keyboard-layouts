454dc17ec1c290ef8c881d61904a08cc88b92f5f
ellreka <ellreka5364@gmail.com>
2023-01-07 06:34:59 +0900

    feat: add ga

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,19 @@
-import type { MetaFunction } from '@remix-run/node'
+import { MetaFunction, json } from '@remix-run/node'
 import {
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration,
+  useLoaderData,
+  useLocation
 } from '@remix-run/react'
+import { useEffect } from 'react'
 import { RecoilRoot } from 'recoil'
 import { siteConfig } from '~/siteConfig'
 import styles from './styles/app.css'
+import * as gtag from '~/utils/gtags.client'
 
 export function links() {
   return [
@@ -37,7 +41,20 @@ export const meta: MetaFunction = () => ({
   'twitter:site': '@ellreka'
 })
 
+export const loader = async () => {
+  return json({ gaTrackingId: process.env.GA_TRACKING_ID })
+}
+
 export default function App() {
+  const location = useLocation()
+  const { gaTrackingId } = useLoaderData<typeof loader>()
+
+  useEffect(() => {
+    if (gaTrackingId?.length) {
+      gtag.pageview(location.pathname, gaTrackingId)
+    }
+  }, [location, gaTrackingId])
+
   return (
     <html lang="en" data-theme="wireframe">
       <head>
@@ -45,6 +62,28 @@ export default function App() {
         <Links />
       </head>
       <body>
+        {process.env.NODE_ENV === 'development' || !gaTrackingId ? null : (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            />
+            <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `
+              }}
+            />
+          </>
+        )}
         <RecoilRoot>
           <Outlet />
         </RecoilRoot>
