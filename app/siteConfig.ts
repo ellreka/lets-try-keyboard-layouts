c873f2fad7dd6865e1fd53fda03cf8487de9812d
ellreka <ellreka5364@gmail.com>
2023-01-11 17:27:01 +0900

    fix: fix site url

--- app/siteConfig.ts
+++ app/siteConfig.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   title: "Let's Try Keyboard Layouts!",
   description: 'Try different keyboard layouts or create your own!',
-  url: 'https://lets-try-keyboard-layouts.vercel.app',
+  url: 'https://lets-try-keyboard-layouts.ellreka.net',
   text: '',
   keywords: ['LetsTryKeyboardLayouts']
 }
